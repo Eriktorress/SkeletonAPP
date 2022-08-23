d794b1e629237f36616749014f682e73236ad1c8
Erik <eri.torres@duocuc.cl>
2022-08-23 16:36:32 -0400

    Login

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  user={
+    name:""
+
+  }
+   
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
