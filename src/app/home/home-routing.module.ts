651362d1e2817db843ba2d95401d67194f3c409b
Erik <eri.torres@duocuc.cl>
2022-09-14 20:52:40 -0300

    children

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CertificacionesComponent } from '../certificaciones/certificaciones.component';
+import { ExperienciaComponent } from '../experiencia/experiencia.component';
+import { MisdatosComponent } from '../misdatos/misdatos.component';
 import { HomePage } from './home.page';
+
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
-  }
+    children:[
+      {
+        path:'/experiencia',
+        component: ExperienciaComponent
+
+      },
+      {
+        path:'/certificaciones',
+        component: CertificacionesComponent
+
+      },
+      {
+        path:'/misdatos',
+        component: MisdatosComponent
+      },
+
+    ]
+  },
 ];
 
 @NgModule({
